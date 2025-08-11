8841203f04d52b7091b83c6ed1675b2d8bb68b04
Sandeep Kumar <vishalsoni2225@gmail.com>
2025-08-11 23:38:13 +0530

    started working on hero section

--- app/page.js
+++ app/page.js
@@ -3,6 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
    <main className="bg-[#020617] text-white min-h-screen">
+      <section id="here" className="hero min-h-screen flex items-center justify-center relative px-4 sm:px-6 lg:px-8 py-10 md:py-16 lg:py-0 hero-section-padding">
+        <p>Hero Section</p>
+      </section>
+      <section id="about" className="hero min-h-screen flex items-center justify-center relative px-4 sm:px-6 lg:px-8 py-10 md:py-16 lg:py-0 hero-section-padding">
+        <p>About Section</p>
+      </section>
+      <section id="skills" className="bg-grey-900 relative min-h-screen w-full mx-auto overflow-hidden">
+        <p>Hero Section</p>
+      </section>
+      <section id="project" className="bg-grey-900 relative min-h-screen w-full mx-auto overflow-hidden">
+        <p>Hero Section</p>
+      </section>
+      <section id="contact" className="bg-grey-900 relative min-h-screen w-full mx-auto overflow-hidden">
+        <p>Hero Section</p>
+      </section>
     </main>
   );
 }
